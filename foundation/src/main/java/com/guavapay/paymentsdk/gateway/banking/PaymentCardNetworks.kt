3a4651378e400a5141c988a95f37850544499bbc
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-06-11 13:16:30 +0300

    Relations between card networks and images added

--- foundation/src/main/java/com/guavapay/paymentsdk/gateway/banking/PaymentCardNetworks.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/gateway/banking/PaymentCardNetworks.kt
@@ -1,5 +1,13 @@
 package com.guavapay.paymentsdk.gateway.banking
 
-enum class PaymentCardNetworks {
-  VISA, MASTERCARD, UNIONPAY, DISCOVER, AMEX, DINERS_CLUB
+import androidx.annotation.DrawableRes
+import com.guavapay.paymentsdk.R
+
+enum class PaymentCardNetworks(@DrawableRes val imageres: Int) {
+  VISA(R.drawable.ic_logo_cn_visa),
+  MASTERCARD(R.drawable.ic_logo_cn_mastercard),
+  UNIONPAY(R.drawable.ic_logo_cn_unionpay),
+  DISCOVER(R.drawable.ic_logo_cn_discover),
+  AMEX(R.drawable.ic_logo_cn_amex),
+  DINERS_CLUB(R.drawable.ic_logo_cn_dinersclub)
 }
