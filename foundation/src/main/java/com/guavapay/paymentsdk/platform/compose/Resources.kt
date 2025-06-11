75871c8f4446b926cc5b45c3db924299968806f6
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-06-11 13:14:17 +0300

    ComposeStringProvider becomes Serializable

--- foundation/src/main/java/com/guavapay/paymentsdk/platform/compose/Resources.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/platform/compose/Resources.kt
@@ -3,9 +3,10 @@ package com.guavapay.paymentsdk.platform.compose
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.ReadOnlyComposable
+import java.io.Serializable
 
-internal typealias ComposeString = ComposeStringProvider
+typealias ComposeString = ComposeStringProvider
 
-@Immutable fun interface ComposeStringProvider {
+@Immutable fun interface ComposeStringProvider : Serializable {
   @ReadOnlyComposable @Composable operator fun invoke(): String
 }
