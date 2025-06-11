344b8b08f3c5cc778cbd93fc52b1fa823ea17ac8
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-06-11 13:14:37 +0300

    PaymentKind becomes serializable

--- foundation/src/main/java/com/guavapay/paymentsdk/gateway/banking/PaymentKind.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/gateway/banking/PaymentKind.kt
@@ -1,10 +1,11 @@
 package com.guavapay.paymentsdk.gateway.banking
 
-import androidx.compose.ui.res.stringResource as string
 import com.guavapay.paymentsdk.R
 import com.guavapay.paymentsdk.platform.compose.ComposeString
+import java.io.Serializable
+import androidx.compose.ui.res.stringResource as string
 
-sealed class PaymentKind(open val text: ComposeString) {
+sealed class PaymentKind(open val text: ComposeString) : Serializable {
   data object Book : PaymentKind({ string(R.string.payment_kind_book) })
   data object Buy : PaymentKind({ string(R.string.payment_kind_buy) })
   data object Checkout : PaymentKind({ string(R.string.payment_kind_checkout) })
