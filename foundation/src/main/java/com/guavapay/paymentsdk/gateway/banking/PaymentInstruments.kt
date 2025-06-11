97ef0f575ecf49a4e46bd56382ec7c758e2c6936
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-06-11 13:15:15 +0300

    PaymentInstruments default value removed + instrument gatherer method added

--- foundation/src/main/java/com/guavapay/paymentsdk/gateway/banking/PaymentInstruments.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/gateway/banking/PaymentInstruments.kt
@@ -2,4 +2,6 @@ package com.guavapay.paymentsdk.gateway.banking
 
 import java.io.Serializable
 
-data class PaymentInstruments(val methods: Set<PaymentMethod> = setOf()) : Serializable
+data class PaymentInstruments(val methods: Set<PaymentMethod>) : Serializable {
+  inline fun <reified T : PaymentMethod> instrument() = methods.find { it is T } as? T
+}
