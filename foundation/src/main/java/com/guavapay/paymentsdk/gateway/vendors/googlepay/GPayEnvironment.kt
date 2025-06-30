a4fb2d701a56f4e06db6a87b6cd6d639b751dfad
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-06-30 13:54:15 +0300

    Integrate new cards page with backend side

--- foundation/src/main/java/com/guavapay/paymentsdk/gateway/vendors/googlepay/GPayEnvironment.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/gateway/vendors/googlepay/GPayEnvironment.kt
@@ -2,7 +2,7 @@ package com.guavapay.paymentsdk.gateway.vendors.googlepay
 
 import com.google.android.gms.wallet.WalletConstants
 
-enum class GPayEnvironment(internal val qualifier: Int) {
+internal enum class GPayEnvironment(internal val qualifier: Int) {
   TEST(WalletConstants.ENVIRONMENT_TEST),
   PRODUCTION(WalletConstants.ENVIRONMENT_PRODUCTION)
 }
