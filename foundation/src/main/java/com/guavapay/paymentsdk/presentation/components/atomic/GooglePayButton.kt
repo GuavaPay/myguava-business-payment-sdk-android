25e15dfe1f537c1cc8756c9dd40213ab05be5a1f
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-07-07 15:45:36 +0300

    Google Pay button refactoring

--- foundation/src/main/java/com/guavapay/paymentsdk/presentation/components/atomic/GooglePayButton.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/presentation/components/atomic/GooglePayButton.kt
@@ -4,36 +4,44 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.pay.button.ButtonTheme
 import com.google.pay.button.ButtonType
-import com.guavapay.paymentsdk.gateway.banking.PaymentMethod
-import com.guavapay.paymentsdk.gateway.banking.PaymentResult
-import com.guavapay.paymentsdk.gateway.launcher.PaymentGatewayState
-import com.guavapay.paymentsdk.gateway.vendors.googlepay.rememberGPayOrchestrator
-import com.guavapay.paymentsdk.gateway.vendors.googlepay.toGoogleAllowedMethodsJson
+import com.guavapay.paymentsdk.gateway.banking.PaymentKind
+import com.guavapay.paymentsdk.gateway.launcher.PaymentGatewayCoroutineScope
+import com.guavapay.paymentsdk.gateway.vendors.googlepay.GPayOrchestrator
+import com.guavapay.paymentsdk.gateway.vendors.googlepay.GPayResult
 import com.guavapay.paymentsdk.presentation.platform.LocalSizesProvider
+import com.guavapay.paymentsdk.presentation.platform.remember
+import com.guavapay.paymentsdk.rememberLibraryUnit
 import kotlinx.coroutines.launch
 
-@Composable internal fun GooglePayButton(state: PaymentGatewayState, result: (PaymentResult) -> Unit) {
+@Composable internal fun GooglePayButton(orchestrator: GPayOrchestrator, result: (GPayResult) -> Unit) {
+  val isReady by orchestrator.isReady.collectAsState()
+  val isProcessing by orchestrator.isProcessing.collectAsState()
   val sizes = LocalSizesProvider.current
-  val scope = rememberCoroutineScope()
-  val orchestrator = rememberGPayOrchestrator(state)
-  val isProcessing by orchestrator.isProcessing.collectAsStateWithLifecycle()
-  val isReady by orchestrator.isReady.collectAsStateWithLifecycle()
 
-  val gpay = remember(state.instruments) { state.instruments.instrument<PaymentMethod.GooglePay>() } ?: return
-  val json = remember(state.instruments, state::toGoogleAllowedMethodsJson)
-  val type = remember(gpay.ordertype) { ButtonType.entries.find { it.value == gpay.ordertype.qualifier } ?: ButtonType.Pay }
+  val type = remember(rememberLibraryUnit().state.payload?.kind) {
+    when (it) {
+      is PaymentKind.Book -> ButtonType.Book
+      is PaymentKind.Buy -> ButtonType.Buy
+      is PaymentKind.Checkout -> ButtonType.Checkout
+      is PaymentKind.Custom -> ButtonType.Plain
+      is PaymentKind.Donate -> ButtonType.Donate
+      is PaymentKind.Order -> ButtonType.Order
+      is PaymentKind.Pay -> ButtonType.Pay
+      is PaymentKind.Plain -> ButtonType.Plain
+      is PaymentKind.Subscribe -> ButtonType.Subscribe
+      null -> ButtonType.Pay
+    }
+  }
 
   com.google.pay.button.PayButton(
-    onClick = { scope.launch { result(orchestrator.start()) } },
-    allowedPaymentMethods = json,
+    onClick = { PaymentGatewayCoroutineScope().launch { result(orchestrator.start()) } },
+    allowedPaymentMethods = orchestrator.buttonPayload,
     modifier = Modifier.fillMaxWidth().height(sizes.button().height),
     theme = if (isSystemInDarkTheme()) ButtonTheme.Light else ButtonTheme.Dark,
     type = type,
