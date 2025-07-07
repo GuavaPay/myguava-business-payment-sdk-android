87299daca877dafa9f0dc8603a1cfa1ac74e2e89
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-07-07 15:27:10 +0300

    Library state updated

--- foundation/src/main/java/com/guavapay/paymentsdk/LibraryState.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/LibraryState.kt
@@ -2,6 +2,11 @@ package com.guavapay.paymentsdk
 
 import com.guavapay.paymentsdk.gateway.launcher.PaymentGatewayPayload
 
-internal class LibraryState(var payload: PaymentGatewayPayload? = null) {
+internal class LibraryState(
+  var payload: PaymentGatewayPayload? = null,
+  var device: Device = Device()
+) {
   inline fun payload() = payload ?: throw IllegalStateException("You can't access to gateway payload until not initialized!")
+
+  data class Device(val ip: String? = null)
 }
