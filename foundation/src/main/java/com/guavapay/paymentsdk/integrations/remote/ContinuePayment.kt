4165e5f54fd7df03f8e6eefbcb76f0ad1a4fea5d
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-07-07 15:38:21 +0300

    Timeout for continue payment added (for 1 min)

--- foundation/src/main/java/com/guavapay/paymentsdk/integrations/remote/ContinuePayment.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/integrations/remote/ContinuePayment.kt
@@ -5,4 +5,4 @@ import com.guavapay.paymentsdk.integrations.RunIntegration
 import com.guavapay.paymentsdk.network.services.OrderApi.Models.ContinuePaymentRequest
 
 internal suspend fun RemoteContinuePayment(lib: LibraryUnit, orderId: String, request: ContinuePaymentRequest) =
-  RunIntegration(lib, 10000) { lib.network.services.order.continuePayment(orderId = orderId, request = request) }
+  RunIntegration(lib, 60000) { lib.network.services.order.continuePayment(orderId = orderId, request = request) }
