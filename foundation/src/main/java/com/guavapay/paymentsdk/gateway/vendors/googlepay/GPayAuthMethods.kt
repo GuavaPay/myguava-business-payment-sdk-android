1778b43bdc3447f5aca7d8b1abb82df5bc538a41
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-08-06 16:11:57 +0300

    Added documentation for GPayAuthMethods enum, detailing authentication methods for Google Pay.

--- foundation/src/main/java/com/guavapay/paymentsdk/gateway/vendors/googlepay/GPayAuthMethods.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/gateway/vendors/googlepay/GPayAuthMethods.kt
@@ -1,3 +1,19 @@
 package com.guavapay.paymentsdk.gateway.vendors.googlepay
 
-enum class GPayAuthMethods { PAN_ONLY, CRYPTOGRAM_3DS }
+/**
+ * Defines the authentication methods for Google Pay.
+ * @author Pavel Erokhin (MairwunNx / GuavaAgent007)
+ */
+enum class GPayAuthMethods {
+  /**
+   * Card authentication will be done using a PAN (Primary Account Number) only.
+   * This is a less secure method.
+   */
+  PAN_ONLY,
+
+  /**
+   * Card authentication will be done using a cryptogram and 3D-Secure.
+   * This is a more secure method.
+   */
+  CRYPTOGRAM_3DS
+}
