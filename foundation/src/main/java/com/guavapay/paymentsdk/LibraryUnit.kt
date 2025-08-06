b565697bd7e66d1d8dc82b98cdfb50c272365964
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-08-06 16:10:48 +0300

    Implemented conditional behavior in rememberLibraryUnit for inspection mode, initializing PaymentGatewayPayload with default values.

--- foundation/src/main/java/com/guavapay/paymentsdk/LibraryUnit.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/LibraryUnit.kt
@@ -3,7 +3,10 @@ package com.guavapay.paymentsdk
 import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.startup.AppInitializer
+import com.guavapay.paymentsdk.gateway.banking.PaymentCircuit
+import com.guavapay.paymentsdk.gateway.launcher.PaymentGatewayPayload
 import com.guavapay.paymentsdk.logging.i
 import com.guavapay.paymentsdk.network.NetworkUnit
 import com.guavapay.paymentsdk.platform.coroutines.CoroutineUnit
@@ -11,7 +14,19 @@ import com.guavapay.paymentsdk.platform.function.lazy
 import com.guavapay.paymentsdk.presentation.platform.remember
 import kotlin.reflect.KProperty0
 
-@Composable internal fun rememberLibraryUnit() = remember(LocalContext.current, LibraryUnit::from)
+@Composable internal fun rememberLibraryUnit() = if (LocalInspectionMode.current) {
+  LibraryUnit(LocalContext.current).apply {
+    state.payload = PaymentGatewayPayload(
+      orderId = "<null>",
+      sessionToken = "<null>",
+      locale = androidx.compose.ui.text.intl.Locale.current.platformLocale,
+      circuit = PaymentCircuit.Development,
+      threedsLooknfeel = null,
+    )
+  }
+} else {
+  remember(LocalContext.current, LibraryUnit::from)
+}
 
 internal class LibraryUnit(val context: Context) {
   val state by lazy(::LibraryState)
