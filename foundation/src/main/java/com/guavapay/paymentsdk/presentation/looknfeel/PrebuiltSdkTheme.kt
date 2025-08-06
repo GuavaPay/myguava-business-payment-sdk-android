2e1cdb0936f80180ab6e9ff91df5430f4faac99f
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-08-06 16:15:54 +0300

    Updated PrebuiltSdkTheme to include LocalSizesProvider and LocalTokensProvider imports in the payment SDK.

--- foundation/src/main/java/com/guavapay/paymentsdk/presentation/looknfeel/PrebuiltSdkTheme.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/presentation/looknfeel/PrebuiltSdkTheme.kt
@@ -4,10 +4,10 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import com.guavapay.paymentsdk.presentation.platform.LocalSizesProvider
-import com.guavapay.paymentsdk.presentation.platform.LocalTokensProvider
 import com.guavapay.paymentsdk.presentation.looknfeel.adjustable.PaymentSdkSizes
 import com.guavapay.paymentsdk.presentation.looknfeel.adjustable.PaymentSdkTokens
+import com.guavapay.paymentsdk.presentation.platform.LocalSizesProvider
+import com.guavapay.paymentsdk.presentation.platform.LocalTokensProvider
 
 @Composable internal fun PrebuiltSdkTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
   val colorScheme = if (darkTheme) PrebuiltDarkColorScheme else PrebuiltLightColorScheme
