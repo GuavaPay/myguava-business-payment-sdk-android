6a950d262cc89193582d0a4957b7f6d3174e0183
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-08-06 16:17:14 +0300

    Added ComposeString and ComposeStringProvider interfaces for improved string composition in the payment SDK.

--- foundation/src/main/java/com/guavapay/paymentsdk/presentation/platform/Resources.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/presentation/platform/Resources.kt
@@ -5,8 +5,18 @@ import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.ReadOnlyComposable
 import java.io.Serializable
 
+/**
+ * Represents a string that can be composed using a ComposeStringProvider.
+ *
+ * @author Pavel Erokhin (MairwunNx / GuavaAgent007)
+ */
 typealias ComposeString = ComposeStringProvider
 
+/**
+ * A functional interface that provides a string via Compose.
+ *
+ * @author Pavel Erokhin (MairwunNx / GuavaAgent007)
+ */
 @Immutable fun interface ComposeStringProvider : Serializable {
   @ReadOnlyComposable @Composable operator fun invoke(): String
 }
