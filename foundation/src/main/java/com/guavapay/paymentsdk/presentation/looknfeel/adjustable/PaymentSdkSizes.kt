7d6a5e7d719817b10a4c129bf5cb7d23c3e9baeb
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-08-06 16:15:58 +0300

    Refactored PaymentSdkSizes to provide default dimensions for UI components

--- foundation/src/main/java/com/guavapay/paymentsdk/presentation/looknfeel/adjustable/PaymentSdkSizes.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/presentation/looknfeel/adjustable/PaymentSdkSizes.kt
@@ -8,8 +8,30 @@ import com.guavapay.paymentsdk.presentation.platform.PaymentGatewaySizesProvider
 import com.guavapay.paymentsdk.presentation.platform.PaymentGatewaySizesProvider.CheckboxSizes
 import com.guavapay.paymentsdk.presentation.platform.PaymentGatewaySizesProvider.TextFieldSizes
 
+/**
+ * Default implementation of [PaymentGatewaySizesProvider] for the Payment SDK.
+ *
+ * This class provides default dimensions for various UI components.
+ * It can be extended to customize the sizes.
+ *
+ * @author Pavel Erokhin (MairwunNx / GuavaAgent007)
+ */
 open class PaymentSdkSizes : PaymentGatewaySizesProvider {
+  /**
+   * Provides the default sizes for a text field.
+   * @return [TextFieldSizes] with a height of 48dp.
+   */
   @ReadOnlyComposable @Composable override fun textfield() = TextFieldSizes(48.dp)
-  @ReadOnlyComposable @Composable override fun button() = ButtonSizes(56.dp)
+
+  /**
+   * Provides the default sizes for a button.
+   * @return [ButtonSizes] with a height of 48dp.
+   */
+  @ReadOnlyComposable @Composable override fun button() = ButtonSizes(48.dp)
+
+  /**
+   * Provides the default sizes for a checkbox.
+   * @return [CheckboxSizes] with a height of 24dp.
+   */
   @ReadOnlyComposable @Composable override fun checkbox() = CheckboxSizes(24.dp)
 }
