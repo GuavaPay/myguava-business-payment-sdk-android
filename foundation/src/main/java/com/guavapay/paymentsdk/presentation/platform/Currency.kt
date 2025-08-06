2fbedc01beca57d9edb95df476fc5e4659bc141c
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-08-06 16:16:30 +0300

    Refactored currencify functions to internal visibility in Currency.kt

--- foundation/src/main/java/com/guavapay/paymentsdk/presentation/platform/Currency.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/presentation/platform/Currency.kt
@@ -5,8 +5,8 @@ import java.util.Currency
 import java.util.Currency.getInstance
 import java.util.Locale
 
-fun currencify(baseUnit: Number, currency: Currency, locale: Locale) =
+internal fun currencify(baseUnit: Number, currency: Currency, locale: Locale) =
   getCurrencyInstance(locale).apply { this.currency = currency }.format(baseUnit)
 
-fun currencify(baseUnit: Number, currency: String, locale: Locale) =
+internal fun currencify(baseUnit: Number, currency: String, locale: Locale) =
   currencify(baseUnit, getInstance(currency), locale)
