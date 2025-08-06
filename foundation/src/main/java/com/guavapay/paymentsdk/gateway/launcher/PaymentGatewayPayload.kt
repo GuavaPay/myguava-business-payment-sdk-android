c158fe356dfe4afc8cbef0c40a6e91d560841f9e
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-08-06 16:11:52 +0300

    Added detailed documentation for PaymentGatewayPayload data class and its properties.

--- foundation/src/main/java/com/guavapay/paymentsdk/gateway/launcher/PaymentGatewayPayload.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/gateway/launcher/PaymentGatewayPayload.kt
@@ -12,6 +12,20 @@ import com.guavapay.myguava.business.myguava3ds2.init.ui.GUiCustomization
 import java.io.Serializable
 import java.util.Locale
 
+/**
+ * Data class that holds all the necessary configuration and data for a payment session.
+ *
+ * @property orderId The unique identifier for the order.
+ * @property sessionToken A token for the user's session.
+ * @property locale The locale to be used for the UI. If null, the system locale will be used.
+ * @property circuit The payment environment to use. If null, the default circuit will be used.
+ * @property kind The kind of payment, which affects the text on the pay button.
+ * @property methods The set of allowed payment methods.
+ * @property schemes The set of allowed card schemes.
+ * @property categories The set of allowed card categories.
+ * @property looknfeel A decorator for the main payment UI.
+ * @author Pavel Erokhin (MairwunNx / GuavaAgent007)
+ */
 data class PaymentGatewayPayload(
   val orderId: String,
   val sessionToken: String,
@@ -24,12 +38,21 @@ data class PaymentGatewayPayload(
   val threedsLooknfeel: GUiCustomization? = null,
   val looknfeel: PaymentGatewayLooknfeel = PaymentGatewayLooknfeel { PrebuiltSdkTheme { it() } },
 ) : Serializable {
+  /**
+   * A functional interface to decorate the payment screen's UI.
+   * @author Pavel Erokhin (MairwunNx / GuavaAgent007)
+   */
   @Stable fun interface PaymentGatewayLooknfeel : Serializable {
+    /**
+     * A composable function that wraps the content of the payment screen.
+     *
+     * @param content The content to be decorated.
+     */
     @Composable fun Decorate(content: @Composable () -> Unit)
   }
 
-  inline fun locale() = locale!! // Never will happen. Initialized by PaymentGateway
-  inline fun threedsLooknfeel() = threedsLooknfeel!! // Never will happen. Initialized by PaymentGateway
+  internal inline fun locale() = locale!! // Never will happen. Initialized by PaymentGateway
+  internal inline fun threedsLooknfeel() = threedsLooknfeel!! // Never will happen. Initialized by PaymentGateway
 
   companion object
 }
