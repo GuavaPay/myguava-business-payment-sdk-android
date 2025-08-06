9aff9c3a99cd7c65e8f68de6db55a9871d77f6df
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-08-06 16:12:03 +0300

    Added detailed documentation for GPayOrderType enum, specifying Google Pay button types and their corresponding constants.

--- foundation/src/main/java/com/guavapay/paymentsdk/gateway/vendors/googlepay/GPayOrderType.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/gateway/vendors/googlepay/GPayOrderType.kt
@@ -9,13 +9,36 @@ import com.google.android.gms.wallet.button.ButtonConstants.ButtonType.PAY
 import com.google.android.gms.wallet.button.ButtonConstants.ButtonType.PLAIN
 import com.google.android.gms.wallet.button.ButtonConstants.ButtonType.SUBSCRIBE
 
+/**
+ * Represents the type of Google Pay button to be displayed.
+ *
+ * This enum maps to the `ButtonType` constants from the Google Pay API.
+ *
+ * @property qualifier The integer qualifier for the Google Pay button type.
+ * @author Pavel Erokhin (MairwunNx / GuavaAgent007)
+ */
 enum class GPayOrderType(internal val qualifier: Int) {
+  /** Corresponds to `ButtonType.BOOK`. */
   Book(BOOK),
+
+  /** Corresponds to `ButtonType.BUY`. */
   Buy(BUY),
+
+  /** Corresponds to `ButtonType.CHECKOUT`. */
   Checkout(CHECKOUT),
+
+  /** Corresponds to `ButtonType.DONATE`. */
   Donate(DONATE),
+
+  /** Corresponds to `ButtonType.ORDER`. */
   Order(ORDER),
+
+  /** Corresponds to `ButtonType.PAY`. */
   Pay(PAY),
+
+  /** Corresponds to `ButtonType.PLAIN`. */
   Plain(PLAIN),
+
+  /** Corresponds to `ButtonType.SUBSCRIBE`. */
   Subscribe(SUBSCRIBE),
 }
