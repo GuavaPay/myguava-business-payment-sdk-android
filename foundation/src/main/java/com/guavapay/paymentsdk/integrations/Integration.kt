02fb95aa0b5e9c3e8073fe583778b4a07926c08c
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-08-06 16:12:12 +0300

    Refactored integration functions to improve timeout handling and added local integration support.

--- foundation/src/main/java/com/guavapay/paymentsdk/integrations/Integration.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/integrations/Integration.kt
@@ -6,14 +6,15 @@ import com.guavapay.paymentsdk.integrations.IntegrationException.NoResponseError
 import com.guavapay.paymentsdk.integrations.IntegrationException.ServerError
 import com.guavapay.paymentsdk.integrations.IntegrationException.TimeoutError
 import com.guavapay.paymentsdk.integrations.IntegrationException.UnqualifiedError
+import com.guavapay.paymentsdk.logging.d
+import com.guavapay.paymentsdk.logging.e
+import com.guavapay.paymentsdk.logging.w
 import com.guavapay.paymentsdk.platform.coroutines.timeouting
 import kotlinx.coroutines.withContext
 import retrofit2.Response
-import com.guavapay.paymentsdk.logging.e
-import com.guavapay.paymentsdk.logging.w
-import com.guavapay.paymentsdk.logging.d
 
-internal suspend inline fun <reified T> RunIntegration(lib: LibraryUnit, crossinline op: suspend () -> Response<T>): T = RunIntegration(lib, 4000, op)
+internal suspend inline fun <reified T> RunLocalIntegration(lib: LibraryUnit, crossinline op: suspend () -> T): T = RunLocalIntegration(lib, 4000, op)
+internal suspend inline fun <reified T> RunIntegration(lib: LibraryUnit, crossinline op: suspend () -> Response<T>): T = RunIntegration(lib, 30_000, op)
 
 internal suspend inline fun <reified T> RunIntegration(lib: LibraryUnit, timeoutMs: Long, crossinline op: suspend () -> Response<T>): T {
   return withContext(lib.coroutine.dispatchers.common + lib.coroutine.elements.named) {
@@ -38,16 +39,42 @@ internal suspend inline fun <reified T> RunIntegration(lib: LibraryUnit, timeout
   }
 }
 
+internal suspend inline fun <reified T> RunLocalIntegration(lib: LibraryUnit, timeoutMs: Long, crossinline op: suspend () -> T): T {
+  return withContext(lib.coroutine.dispatchers.common + lib.coroutine.elements.named) {
+    d("Starting local integration operation for gathering ${T::class.simpleName}")
+
+    timeouting<T, TimeoutError>(
+      timeoutMs = timeoutMs,
+      factory = { cause ->
+        e("Local integration operation for gathering ${T::class.simpleName} timed out", cause)
+        TimeoutError("local operation timed out", cause)
+      }
+    ) {
+      runCatching {
+        op()
+      }.onFailure {
+        e("An error occurred while executing the local integration operation for gathering ${T::class.simpleName}", it)
+        lib.coroutine.handlers.logcat.handler(coroutineContext, it)
+        lib.coroutine.handlers.metrica.handler(coroutineContext, it)
+      }.getOrThrow()
+    }
+  }
+}
+
 internal sealed class IntegrationException(message: String, cause: Throwable? = null) : Exception(message, cause) {
-  object NoResponseError : IntegrationException("No response from server (204)", null)
+  class NoResponseError : IntegrationException("No response from server (204)") { private fun readResolve(): Any = NoResponseError() }
   class TimeoutError(message: String, cause: Throwable? = null) : IntegrationException(message, cause)
   class ClientError(override val message: String, val code: Int, cause: Throwable? = null) : IntegrationException(message, cause)
   class ServerError(override val message: String, val code: Int, cause: Throwable? = null) : IntegrationException(message, cause)
   class UnqualifiedError(message: String, cause: Throwable? = null) : IntegrationException(message, cause)
 }
 
-private fun <T> map(response: Response<T>) = when {
-  response.isSuccessful -> response.body() ?: if (response.code() == 204) throw NoResponseError else throw UnqualifiedError("no response when expected")
+private inline fun <reified T> map(response: Response<T>) = when {
+  response.isSuccessful -> response.body() ?: when {
+    response.code() == 204 -> throw NoResponseError()
+    T::class == Unit::class -> Unit as T
+    else -> throw UnqualifiedError("no response when expected")
+  }
   response.code() in 400..499 -> throw ClientError(message = response.message().ifBlank { "Client error" }, code = response.code())
   response.code() in 500..599 -> throw ServerError(message = response.message().ifBlank { "Server error" }, code = response.code())
   else -> throw UnqualifiedError("returned unexpected HTTP ${response.code()}: ${response.message()}")
