4954ffaf23046d988c24fcdc2cbc8ece68dfabc7
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-08-11 15:38:45 +0300

    Implemented coroutinify utility for ViewModel, enhancing coroutine management and exception handling in SDK.

--- foundation/src/main/java/com/guavapay/paymentsdk/presentation/platform/ViewModel.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/presentation/platform/ViewModel.kt
@@ -5,10 +5,23 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider.Factory
 import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.guavapay.paymentsdk.LibraryUnit
+import com.guavapay.paymentsdk.platform.coroutines.CompositeExceptionHandler
+import com.guavapay.paymentsdk.platform.function.lazy
 import com.guavapay.paymentsdk.rememberLibraryUnit
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
 internal fun interface ViewModelFactory<T : ViewModel> : Factory {
   fun create(extras: CreationExtras): T
@@ -63,3 +76,49 @@ internal fun interface ViewModelFactory<T : ViewModel> : Factory {
     ctor(lib, handle, a, b, c)
   })
 }
+
+internal class ViewModel0 /* Я не придумал лучше названия, но в целом пойдет:). */ (private val vm: ViewModel, private val lib: LibraryUnit) {
+  private val handler = CompositeExceptionHandler(lib.coroutine.handlers.logcat, lib.coroutine.handlers.metrica)
+
+  val scope by lazy { CoroutineScope(vm.viewModelScope.coroutineContext + handler) }
+
+  private fun merged(
+    external: CoroutineContext,
+    extra: CoroutineExceptionHandler?
+  ): CoroutineExceptionHandler? {
+    val base = scope.coroutineContext[CoroutineExceptionHandler]
+    val external = external[CoroutineExceptionHandler]
+    val list = listOfNotNull(base, external, extra)
+    return when (list.size) {
+      0 -> null
+      1 -> list.first()
+      else -> CompositeExceptionHandler(*list.toTypedArray())
+    }
+  }
+
+  inline fun launch(
+    context: CoroutineContext = EmptyCoroutineContext,
+    handler: CoroutineExceptionHandler? = null,
+    crossinline block: suspend CoroutineScope.() -> Unit
+  ) = run {
+    val handlers = context.minusKey(CoroutineExceptionHandler)
+    val merged = merged(context, handler)
+    scope.launch(lib.coroutine.elements.named + handlers + (merged ?: EmptyCoroutineContext)) {
+      block()
+    }
+  }
+
+  inline fun <T> async(
+    context: CoroutineContext = EmptyCoroutineContext,
+    crossinline block: suspend CoroutineScope.() -> T
+  ) = scope.async(lib.coroutine.elements.named + context) { block() }
+
+  fun <T> Flow<T>.launch() = launchIn(CoroutineScope(scope.coroutineContext + lib.coroutine.elements.named))
+}
+
+private class CoroutinifyDelegate(private val vm: ViewModel, private val lib: LibraryUnit) : ReadOnlyProperty<ViewModel, ViewModel0> {
+  private var cached: ViewModel0? = null
+  @Synchronized override fun getValue(thisRef: ViewModel, property: KProperty<*>) = cached ?: ViewModel0(thisRef, lib).also { cached = it }
+}
+
+internal fun coroutinify(vm: ViewModel, lib: LibraryUnit): ReadOnlyProperty<ViewModel, ViewModel0> = CoroutinifyDelegate(vm, lib)
