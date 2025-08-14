d61cec8b5d4d739bb414b9699803720c7ac5947d
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-08-14 16:42:25 +0300

    Refactored CardEditScreen to utilize LocalScrollState and improved IME handling for enhanced user experience.

--- foundation/src/main/java/com/guavapay/paymentsdk/presentation/screens/management/CardEditScreen.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/presentation/screens/management/CardEditScreen.kt
@@ -3,27 +3,14 @@
 package com.guavapay.paymentsdk.presentation.screens.management
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.ime
-import androidx.compose.foundation.layout.imeNestedScroll
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.windowInsetsBottomHeight
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -32,17 +19,17 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewLightDark
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.guavapay.paymentsdk.R
+import com.guavapay.paymentsdk.presentation.LocalScrollState
 import com.guavapay.paymentsdk.presentation.components.atoms.Button
 import com.guavapay.paymentsdk.presentation.components.atoms.TextField
 import com.guavapay.paymentsdk.presentation.navigation.Route
 import com.guavapay.paymentsdk.presentation.navigation.Route.CardEditRoute
 import com.guavapay.paymentsdk.presentation.navigation.rememberNavBackStack
 import com.guavapay.paymentsdk.presentation.platform.PreviewTheme
+import com.guavapay.paymentsdk.presentation.platform.ime
 import com.guavapay.paymentsdk.presentation.platform.rememberViewModel
 import com.guavapay.paymentsdk.presentation.platform.string
 import com.guavapay.paymentsdk.presentation.screens.Screen
@@ -51,22 +38,18 @@ import io.sentry.compose.SentryModifier.sentryTag
 import io.sentry.compose.SentryTraced
 import java.io.Serializable
 
-@OptIn(ExperimentalLayoutApi::class)
 internal object CardEditScreen : Screen<CardEditRoute, Actions> {
   data class Actions(val close: () -> Unit = @JvmSerializableLambda {}) : Serializable { private fun readResolve(): Any = Actions() }
 
   @Composable override fun invoke(nav: SnapshotStateList<Route>, route: CardEditRoute, actions: Actions) = SentryTraced("card-edit-screen") {
     val vm = rememberViewModel(::CardEditVM, route)
     val state = vm.state.collectAsStateWithLifecycle()
-    val scroll = rememberScrollState()
+    val parent = LocalScrollState.current
 
     Column(
       modifier = Modifier
         .fillMaxWidth()
-        .verticalScroll(scroll)
         .padding(horizontal = 16.dp, vertical = 24.dp)
-        .imePadding()
-        .imeNestedScroll()
     ) {
       Text(
         text = stringResource(R.string.rename_saved_card),
@@ -90,7 +73,7 @@ internal object CardEditScreen : Screen<CardEditRoute, Actions> {
           actions.close()
           route.onEditConfirmed(route.cardId, state.value.cardName)
         },
-        modifier = Modifier.sentryTag("card-name-input")
+        modifier = Modifier.sentryTag("card-name-input").ime(parent, extraSpace = 16.dp)
       )
 
       Spacer(modifier = Modifier.height(16.dp))
