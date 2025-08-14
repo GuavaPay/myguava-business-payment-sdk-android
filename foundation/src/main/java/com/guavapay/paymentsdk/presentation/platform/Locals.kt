2de368d90216e4ae42a13bf20ece1716a049159f
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-08-14 16:42:33 +0300

    Added LocalParentScrollState to manage scroll state in composition.

--- foundation/src/main/java/com/guavapay/paymentsdk/presentation/platform/Locals.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/presentation/platform/Locals.kt
@@ -1,5 +1,8 @@
 package com.guavapay.paymentsdk.presentation.platform
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.runtime.staticCompositionLocalOf
 
 inline fun <reified T> requiredStaticCompositionLocalOf() = staticCompositionLocalOf<T> { error("No ${T::class.simpleName} instances provided") }
+
+internal val LocalParentScrollState = requiredStaticCompositionLocalOf<ScrollState>()
