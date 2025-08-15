96f0f58a0e8e3b1f453202cce7b994002004c46d
Pavel Erokhin <pavel.yerokhin@guavapay.com>
2025-08-15 15:11:12 +0300

    Refactored navigation routes by removing unnecessary callback parameters for cleaner route definitions.

--- foundation/src/main/java/com/guavapay/paymentsdk/presentation/navigation/PaymentRoutes.kt
+++ foundation/src/main/java/com/guavapay/paymentsdk/presentation/navigation/PaymentRoutes.kt
@@ -6,8 +6,8 @@ internal sealed interface Route : Serializable {
   data object HomeRoute : Route { private fun readResolve(): Any = HomeRoute }
   data class AbortRoute(val throwable: Throwable? = null) : Route
   data object CancelRoute : Route { private fun readResolve(): Any = CancelRoute }
-  data class CardRemoveRoute(val cardId: String, val cardName: String = "", val onDeleteConfirmed: (String) -> Unit = @JvmSerializableLambda {}) : Route
-  data class CardEditRoute(val cardId: String, val cardName: String = "", val onEditConfirmed: (String, String) -> Unit = @JvmSerializableLambda { _, _ -> }) : Route
-  data class ContactRoute(val countryIso: String? = null, val callback: (email: String?, /* or */ phone: String?) -> Unit = @JvmSerializableLambda { _, _ -> }) : Route
-  data class PhoneRoute(val callback: (countryCode: String, countryIso: String) -> Unit = @JvmSerializableLambda { _, _ -> }) : Route
+  data class CardRemoveRoute(val cardId: String, val cardName: String = "") : Route
+  data class CardEditRoute(val cardId: String, val cardName: String = "") : Route
+  data class ContactRoute(val countryIso: String? = null) : Route
+  data object PhoneRoute : Route { private fun readResolve(): Any = PhoneRoute }
 }
